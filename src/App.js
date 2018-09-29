1c18f1156be2430be8db4375581f9b7cfef810d9
solisbrayan <solisbryan138@gmail.com>
2018-09-29 17:37:56 -0500

    messanger

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import Navigation from './components/navigation/Navigation';
+import Logo from './components/logo/Logo';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+      <Navigation />
+      <Logo />
       </div>
     );
   }
