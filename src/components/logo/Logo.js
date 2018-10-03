3bcd7b28ad37cf27cc6747fb12b5d6eed16a238f
solisbrayan <solisbryan138@gmail.com>
2018-10-03 14:23:54 -0500

    worked on logo.css component

--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Tilt from 'react-tilt';
+import crown from './crown.png'
+import './logo.css';
 
 const Logo = () => {
   return(
   <div className= 'ma4 mt0 '>
-    <Tilt className="Tilt" options={{ max : 25 }} style={{ height: 150, width: 150 }} >
-     <div className="Tilt-inner"> 👽 </div>
+    <Tilt className="Tilt br2 shadow-2 " options={{ max : 25 }} style={{ height: 150, width: 150 }} >
+     <div className="Tilt-inner" ><img src={crown}/></div>
     </Tilt>
   </div>
   );
